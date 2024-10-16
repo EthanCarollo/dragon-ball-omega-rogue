88e022c8c5ce6f4eaad23a87baadee8d2c851b75
Ethan Carollo <etcarollo@gmail.com>
2024-10-16 10:32:57 +0200

    🤡 add background

--- src/scenes/menu_scene.rb
+++ src/scenes/menu_scene.rb
@@ -3,12 +3,14 @@ class MenuScene
   def initialize(window)
     @window = window
     @font = Gosu::Font.new(20)
+    @dragon_ball_title = Gosu::Image.new("./assets/title/dbrogue.png", retro: true)
+    @namek_background = Gosu::Image.new("./assets/background/namek_background.png", retro: true)
 
     # Create a button using the factory with a hover color and callback for scene change
     @button_factory = ButtonFactory.new(@window)
     @start_button = @button_factory.create_default_button(
       x: (@window.width - 200) / 2,  # Centering the button
-      y: (@window.height - 50) / 2,
+      y: (@window.height - 50) / 2 + 200,
       width: 200,
       height: 50,
       text: "Start"
@@ -23,7 +25,17 @@ class MenuScene
   end
 
   def draw
-    Gosu.draw_rect(0, 0, @window.width, @window.height, Gosu::Color::BLACK)
+    background_width = @namek_background.width / @window.width
+    background_height = @namek_background.height / @window.height
+    @namek_background.draw(0, 0, 0, background_width, background_height)
+
+    wanted_width = 400*1.5.to_f
+    wanted_height = 270*1.5.to_f
+    title_width = wanted_width / @dragon_ball_title.width
+    title_height = wanted_height / @dragon_ball_title.height
+    puts title_height
+    x_tit_pos = @window.width / 2 - wanted_width / 2
+    @dragon_ball_title.draw(x_tit_pos, 0, 1, title_width, title_height)
     @start_button.draw
   end
 
--- src/scenes/reward_scene.rb
+++ src/scenes/reward_scene.rb
@@ -3,6 +3,21 @@ class RewardScene
   def initialize(window)
     @window = window
     @font = Gosu::Font.new(20)
+
+    @button_factory = ButtonFactory.new(window)
+    text_to_show = "COOL !"
+    text_width = @font.text_width(text_to_show) + 50
+    text_x = @window.width / 2 - text_width / 2
+    text_y = 50
+    @next_button = @button_factory.create_default_button(
+        x: (@window.width - 200) / 2, 
+        y: (@window.height - 50) / 2,
+        width: 200,
+        height: 50,
+        text: "GOOD !"
+      ) do
+        @window.change_scene(GameScene.new(@window))
+      end
   end
 
   def update
@@ -10,10 +25,10 @@ class RewardScene
   end
 
   def draw
-
+    @next_button.draw
   end
 
   def button_down(id)
-
+    @next_button.button_down(id)
   end
 end
